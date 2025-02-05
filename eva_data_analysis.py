89d7bef172a594c101cf870ec35c3bbe41bebc99
Nadine Jacobsen <nadine.jacobsen@outlook.de>
2025-02-05 14:47:23 +0100

    add comments to describe procedure and goal of analysis

--- eva_data_analysis.py
+++ eva_data_analysis.py
@@ -1,17 +1,19 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
+# Plot extra-vehicular activity (eva) of nasa over time
+
 # Data source: https://data.nasa.gov/resource/eva.json (with modifications)
 input_file = open('./eva-data.json', 'r')
 output_file = open('./eva-data.csv', 'w')
 graph_file = './cumulative_eva_graph.png'
 
 eva_df = pd.read_json(input_file, convert_dates=['date'])
-eva_df['eva'] = eva_df['eva'].astype(float)
-eva_df.dropna(axis=0, inplace=True) # drop missing values
-eva_df.sort_values('date', inplace=True)
+eva_df['eva'] = eva_df['eva'].astype(float)  # float needed for calculations
+eva_df.dropna(axis=0, inplace=True)  # drop missing values
+eva_df.sort_values('date', inplace=True)  # sort data by date to calculate cumulative eva
 
-eva_df.to_csv(output_file, index=False) #no headers
+eva_df.to_csv(output_file, index=False)  # save data without headers
 
 eva_df['duration_hours'] = eva_df['duration'].str.split(":").apply(lambda x: int(x[0]) + int(x[1])/60) # convert duration in hours as float
 eva_df['cumulative_time'] = eva_df['duration_hours'].cumsum() 
