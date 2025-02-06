7938ede6f5d0b2e275609fd38e9f2a51f26ea4e2
Nadine Jacobsen <nadine.jacobsen@outlook.de>
2025-02-06 12:11:08 +0100

    incorporate crew test into other tests and parametrize

--- tests/test_eva_data_analysis.py
+++ tests/test_eva_data_analysis.py
@@ -1,5 +1,6 @@
 import pytest
 from eva_data_analysis import text_to_duration
+from eva_data_analysis import calculate_crew_size
 
 def test_text_to_duration_float():
     """
@@ -13,3 +14,24 @@ def test_text_to_duration_integer():
     Test that text_to_duration returns expected value for durations with typical whole hour durations
     """
     assert text_to_duration("10:00") == 10
+
+
+@pytest.mark.parametrize("input_value, expected_results",[
+    ("Mike Foale;",1),
+    ("Joe Tanner;Heide Stefanyshyn-Piper;",2)
+])
+def test_eva_data_analysis_crew_size(input_value, expected_results): 
+    """
+    Test that the calculate_crew_size returns expected ground truth values for crew values
+    """
+    # Typical value 1
+    actual_result =  calculate_crew_size(input_value)
+    assert actual_result == expected_result
+        
+    # Edge case
+    """                                                                                                                               
+    Test that calculate_crew_size returns expected ground truth values                                                                
+    for edge case where crew is an empty string                                                                                       
+    """                                                                                                                               
+    actual_result = calculate_crew_size("")                                                                                           
+    assert actual_result is None 
